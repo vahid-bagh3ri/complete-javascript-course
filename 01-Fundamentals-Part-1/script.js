970017e41064c19f7120f0757dcec6e0fd52ae0e
-Vhd <vahid-bagheri@outlook.com>
2021-12-04 00:04:26 +0330

    5. Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript Is Cool");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Vahid");
+console.log(23);
+
+// camelCase is a standard way to name variables in javaScript
+let firstName = "Vahid";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let BAGHER_SDASDSA = "JD";
+let $function = 27;
+
+let person = "vahid";
+let PI = 3.1415;
+
+let myFirstJob = "developer";
+let mySecondJob = "teacher";
+
+let job1 = "developer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
